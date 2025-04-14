f82552068b97dde9e66c45b604038be6566d1d3c
lhc-dejesusron <r.dejesus@lionheart.asia>
2025-04-14 14:08:56 +0800

    wip cookies

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -13,8 +13,8 @@ const generateToken = (res: Response, userId: string) => {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
       sameSite: 'strict', // Prevent CSRF attacks
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-      // maxAge: 60 * 1000, // 1 minute
+      // maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      maxAge: 60 * 1000, // 1 minute
     });
   } else {
     res.status(400);
